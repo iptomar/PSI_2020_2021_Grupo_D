75679e4f92f945cf2a05701562ed7038a1772477
Rodrigo <rodrigodomingospereira99@gmail.com>
2021-03-16 20:08:15 +0000

    Mapa adicionado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import logo from './logo.png'
 import './App.css';
+import { GoogleMap, Marker, InfoWindow, useLoadScript } from '@react-google-maps/api';
+import {useState,useCallback} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//tamanho e style do map
+const containerStyle = {
+  width: '50vw',
+  height: '50vh'
+};
+
+//centro do mapa quando o mapa faz load
+const center = {
+  lat: 39.23,
+  lng: -8.68,
+};
+
+const options = {
+  disableDefaultUI: false,
 }
 
-export default App;
+//?nao faz nada mas acho que é para carregar da bd tambem (?? talvez)
+const libraries = ["places"];
+
+
+
+
+const App = () => {
+  // state que controla os marcadores
+  // aqui se calhar vai ser carregado da base de dados
+  const [markers, setMarkers] = useState([{
+    lat: null,
+    long : null,
+    time : new Date(),
+  }]);
+
+  //api key aqui
+  const {isLoaded,loadError} = useLoadScript({
+    googleMapsApiKey: "AIzaSyBOxAahL77d5AtjV-Ijxf7p8jmff6MMGMA",
+    libraries,
+  })
+
+  // se isLoaded é true mete o component to googleMaps, se não escreve Loading Maps
+  return isLoaded ? (
+    <div>
+      <h1> Avé Maria <img src={logo} alt=""/> </h1>
+      <GoogleMap 
+      mapContainerStyle = {containerStyle}
+      zoom = {10}
+      center = {center}
+      options = {options}
+      onClick = {(e) => {
+        const newMarker = {
+          lat : e.latLng.lat(),
+          lng : e.latLng.lng(),
+          time : new Date(),
+        }
+        setMarkers([...markers,newMarker])
+      }}
+      >
+      {markers.map((marker) => (
+        //aqui faz o render dos marcadores anteriores e os que sejam a clicar
+        <Marker 
+          key={marker.time.toISOString()} 
+          position = {{lat: marker.lat, lng: marker.lng }} 
+          //icon do marker pode ser mudado : icon = {}
+        />
+        ))}
+          </GoogleMap>
+    </div>
+  ) : <div>Loading Maps...</div>
+}
+
+
+export default App
+
