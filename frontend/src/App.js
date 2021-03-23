926982e4225c7c9f9d6dae7ce99c3f5a3e1bd0d4
Rodrigo <rodrigodomingospereira99@gmail.com>
2021-03-23 01:41:42 +0000

    added infoWindows and fixed some things

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.png'
-import './App.css';
-import { GoogleMap, Marker, InfoWindow, useLoadScript } from '@react-google-maps/api';
-import {useState,useCallback} from 'react';
+import logo from "./logo.png";
+import "./App.css";
+import {
+  GoogleMap,
+  Marker,
+  InfoWindow,
+  useLoadScript,
+} from "@react-google-maps/api";
+import { useState, useCallback } from "react";
 
 //tamanho e style do map
 const containerStyle = {
-  width: '50vw',
-  height: '50vh'
+  width: "100vw",
+  height: "100vh",
 };
 
 //centro do mapa quando o mapa faz load
@@ -17,60 +22,117 @@ const center = {
 
 const options = {
   disableDefaultUI: false,
-}
+};
 
 //?nao faz nada mas acho que é para carregar da bd tambem (?? talvez)
 const libraries = ["places"];
 
-
-
-
 const App = () => {
   // state que controla os marcadores
   // aqui se calhar vai ser carregado da base de dados
-  const [markers, setMarkers] = useState([{
-    lat: null,
-    long : null,
-    time : new Date(),
-  }]);
+  const [markers, setMarkers] = useState([
+    {
+      id: 0,
+      lat: 39.23265937384665,
+      lng: -9.084434204101552,
+      desc: "Marker 1",
+    },
+    {
+      id: 1,
+      lat: 39.176259071456194,
+      lng: -8.765144042968739,
+      desc: "Marker 2",
+    },
+    {
+      id: 2,
+      lat: 39.176259071456194,
+      lng: -8.105141442968739,
+      desc: "Marker 3",
+    },
+    {
+      id: 3,
+      lat: 39.176259071456194,
+      lng: -8.565144242968739,
+      desc: "Marker 4",
+    },
+  ]);
+
+  const onMapClick = useCallback((e) => {
+    const newMarker = {
+      id: markers.length + 1,
+      lat: e.latLng.lat(),
+      lng: e.latLng.lng(),
+    };
+    setMarkers([...markers, newMarker]);
+  }, []);
+
+  const [selectedMarker, setselectedMarker] = useState(null);
 
   //api key aqui
-  const {isLoaded,loadError} = useLoadScript({
+  const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: "AIzaSyBOxAahL77d5AtjV-Ijxf7p8jmff6MMGMA",
     libraries,
-  })
+  });
 
   // se isLoaded é true mete o component to googleMaps, se não escreve Loading Maps
   return isLoaded ? (
     <div>
-      <h1> Avé Maria <img src={logo} alt=""/> </h1>
-      <GoogleMap 
-      mapContainerStyle = {containerStyle}
-      zoom = {10}
-      center = {center}
-      options = {options}
-      onClick = {(e) => {
-        const newMarker = {
-          lat : e.latLng.lat(),
-          lng : e.latLng.lng(),
-          time : new Date(),
-        }
-        setMarkers([...markers,newMarker])
-      }}
+      <h1 className="logo">
+        Avé Maria <img src={logo} alt="" />
+      </h1>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        zoom={10}
+        center={center}
+        options={options}
+        onClick={onMapClick}
       >
-      {markers.map((marker) => (
-        //aqui faz o render dos marcadores anteriores e os que sejam a clicar
-        <Marker 
-          key={marker.time.toISOString()} 
-          position = {{lat: marker.lat, lng: marker.lng }} 
-          //icon do marker pode ser mudado : icon = {}
-        />
+        {markers.map((marker) => (
+          //aqui faz o render dos marcadores anteriores e os que sejam a clicar
+          <Marker
+            key={marker.id}
+            position={{ lat: marker.lat, lng: marker.lng }}
+            //passa o objeto marker para o state dos markers
+            onClick={() => {
+              setselectedMarker(marker);
+            }}
+
+            //icon do marker pode ser mudado : icon = {}
+          ></Marker>
         ))}
-          </GoogleMap>
+        {selectedMarker && (
+          //Info aqui. O 0.045 é para fazer com que a window fiquem em cima do marker.
+          // LOREM IPSUM LOGO AQUI LETS GOOOOOOOOOOOO
+          <InfoWindow
+            onCloseClick={() => {
+              setselectedMarker(null);
+            }}
+            position={{
+              lat: selectedMarker.lat,
+              lng: selectedMarker.lng,
+            }}
+          >
+            <div>
+              {/*O state do selected markers tem toda a informaçao do marker por isso é so meter*/}
+              <h1>{selectedMarker.desc}</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+              <img src={logo} alt="" alignItems="center" />
+            </div>
+          </InfoWindow>
+        )}
+      </GoogleMap>
     </div>
-  ) : <div>Loading Maps...</div>
-}
+  ) : (
+    <div>Loading Maps...</div>
+  );
+};
 
-
-export default App
-
+export default App;
