b7c9977b1963ec559775fa875a44dbdbc8d92514
NoPalm0il <jpedrocr2000@hotmail.com>
2021-04-06 17:01:42 +0100

    points showing up on map

--- frontend/src/core/App.js
+++ frontend/src/core/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import Map from "../pages/map/Map.js";
 import React, { Component } from "react";
-import services from "../services";
 
 export default class App extends Component {
   constructor(props) {
@@ -9,19 +8,6 @@ export default class App extends Component {
     this.state = {
       points: [],
     };
-  }
-
-  //TODO: isto apenas serve de exemplo, modificar isto na classe Map
-  componentDidMount() {
-    services.map
-      .getPoints()
-      .then((points) => {
-        console.log(points);
-        this.setState({
-          points,
-        });
-      })
-      .catch((e) => console.error(e));
   }
 
   render() {
