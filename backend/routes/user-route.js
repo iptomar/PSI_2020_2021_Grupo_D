56fa2bccefa3a7a580f7eec886b7f4d04b807c46
AntonioValerio <antoni_valerio@hotmail.com>
2021-04-25 21:28:39 +0100

    Added Form Component

--- backend/routes/user-route.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const userController = require("../controllers/user-controller");
-
-router.post("/register", userController.register);
-router.post("/login", userController.login);
-
-module.exports = router;
