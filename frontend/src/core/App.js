f000c0c5a18728a200c831ff5ec37cff40f8e537
NoPalm0il <jpedrocr2000@hotmail.com>
2021-04-27 19:05:53 +0100

    added open street map

--- frontend/src/core/App.js
+++ frontend/src/core/App.js
@@ -1,19 +1,9 @@
 import "./App.css";
-import Map from "../pages/map/Map.js";
 import React, { Component } from "react";
-import services from "../services";
 import ReactRouterSetup from "../pages/navroute/Router";
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      points: [],
-    };
-  }
-
   render() {
-    //return <Map />;
-    return <ReactRouterSetup></ReactRouterSetup>;
+    return <ReactRouterSetup />;
   }
 }
--- frontend/src/pages/map/Map.js
+++ frontend/src/pages/map/Map.js
@@ -1,138 +1,22 @@
-import {
-  GoogleMap,
-  Marker,
-  InfoWindow,
-  useLoadScript,
-} from "@react-google-maps/api";
-//import css
-import "./map.css";
-//import global do logo
-import { useState, useCallback, useEffect } from "react";
-import logo from "../../assets/logo.png";
-import services from "../../services";
+import "./Map.css"
 
-//centro do mapa quando o mapa faz load
-const center = {
-  lat: 39.23,
-  lng: -8.68,
-};
+import React, { Component } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
-//tamanho e style do map
-// width: "100vw",
-//   height: "100vh"
-//   ^ isto é mais correto porque nao esconde o zoom in e o zoom out do mapa mas depois fica com scroll. dar fix mais tarde
-const containerStyle = {
-  width: "100vmax",
-  height: "100vmax",
-};
-
-const options = {
-  disableDefaultUI: false,
-};
-
-const Map = () => {
-  // state que controla os marcadores
-  // aqui se calhar vai ser carregado da base de dados
-  const [markers, setMarkers] = useState([]);
-
-  useEffect(() => {
-    async function getPoints() {
-      await services.map
-        .getPoints()
-        .then((points) => {
-          setMarkers(points);
-          console.log(points);
-        })
-        .catch((e) => {
-          console.error(e);
-        });
-    }
-    getPoints();
-  }, []);
-
-  //TODO: this removes markers
-  const onMapClick = useCallback((e) => {
-    const newMarker = {
-      _id: markers.length + 1,
-      lat: e.latLng.lat(),
-      lng: e.latLng.lng(),
-    };
-    setMarkers([...markers, newMarker]);
-  });
-
-  const [selectedMarker, setselectedMarker] = useState(null);
-
-  //api key aqui
-  const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: "AIzaSyBOxAahL77d5AtjV-Ijxf7p8jmff6MMGMA",
-    markers,
-  });
-
-  // se isLoaded é true mete o component to googleMaps, se não escreve Loading Maps
-  //onde é que tens a className logo? é da api? @rodrigo
-  return isLoaded ? (
-    <div>
-      {/* para a side bar ?<div className="titulo-caixa">
-      <img src={logo} alt="" />
-      </div> */}
-      {/* navbar --------- */}
-
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        zoom={10}
-        center={center}
-        options={options}
-        onClick={onMapClick}
-      >
-        {markers.map((marker) => (
-          //aqui faz o render dos marcadores anteriores e os que sejam a clicar
-          <Marker
-            key={marker._id}
-            position={{ lat: marker.lat, lng: marker.lng }}
-            //passa o objeto marker para o state dos markers
-            onClick={() => {
-              setselectedMarker(marker);
-            }}
-
-            //icon do marker pode ser mudado : icon = {}
-          />
-        ))}
-        {selectedMarker && (
-          //Info aqui. O 0.045 é para fazer com que a window fiquem em cima do marker.
-          // LOREM IPSUM LOGO AQUI LETS GOOOOOOOOOOOO
-          <InfoWindow
-            onCloseClick={() => {
-              setselectedMarker(null);
-            }}
-            position={{
-              lat: selectedMarker.lat,
-              lng: selectedMarker.lng,
-            }}
-          >
-            <div>
-              {/*O state do selected markers tem toda a informaçao do marker por isso é so meter*/}
-
-              <h1>{selectedMarker.desc}</h1>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
-              <img src={logo} alt="" alignItems="center" />
-            </div>
-          </InfoWindow>
-        )}
-      </GoogleMap>
-    </div>
-  ) : loadError ? (
-    <div>error</div>
-  ) : (
-    <div>Loading Maps...</div>
-  );
-};
-
-export default Map;
+export default class Map extends Component {
+  render() {
+    return (
+      <MapContainer center={[39.60199, -8.40924]} zoom={13} scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[39.60199, -8.40924]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    );
+  }
+}
