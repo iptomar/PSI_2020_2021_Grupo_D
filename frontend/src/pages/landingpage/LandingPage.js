9870cf3ca28c48e60058c42cb650a8b569931272
JJacintoDev <ouijhg@gmail.com>
2021-05-10 17:45:14 +0100

    Inicio branch

---
+++ frontend/src/pages/landingpage/LandingPage.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import logo_cut from "../../assets/logo.png";
+import "./landingpage.css";
+
+const landingpage = () => {
+  return (
+    <div className="centered">
+      <h1>Em construção</h1>
+      <a href=""></a>
+      <center>
+        <br></br>
+        <br></br>
+        <img src={logo_cut} alt="" />
+      </center>
+    </div>
+  );
+};
+
+export default landingpage;
