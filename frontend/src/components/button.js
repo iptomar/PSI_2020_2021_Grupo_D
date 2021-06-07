a5ceb6775ac8f90a1a97ddef624310d645498966
Rodrigo <rodrigodomingospereira99@gmail.com>
2021-06-07 18:45:58 +0100

    added loader, changed add button

--- frontend/src/components/button.js
+++ frontend/src/components/button.js
@@ -2,9 +2,14 @@ import React from 'react'
 import { Button, Icon } from 'semantic-ui-react'
 
 const btnToggleForm = ({toggleForm}) => (
-    <Button primary onClick = {toggleForm}>
-    <Icon name='plus' /> Adicione a sua experiência
-    </Button>
+    <Button 
+        primary 
+        onClick = {toggleForm}
+        icon = 'plus'
+        labelPosition='right'
+        content = 'Adicione a sua experiência'
+    /> 
+
 )
 
 export default btnToggleForm;
