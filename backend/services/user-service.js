75abb2a19d657fc97db79e72285cd3d810d3006a
NoPalm0il <jpedrocr2000@hotmail.com>
2021-06-07 18:11:32 +0100

    changed password encprytion method

--- backend/services/user-service.js
+++ backend/services/user-service.js
@@ -1,16 +1,21 @@
 const db = require("../configs/mongodb").getDB();
-const cipher = require("../helpers/cipher");
+const bcrypt = require("bcrypt");
 
 //Função que permite o registo do utlizador na tabela "users"
 exports.register = (email, rawPassword) => {
   return new Promise((resolve, reject) => {
     try {
-      let tokeniv = cipher.generateIv();
-      let password = cipher.encrypt(rawPassword, tokeniv);
-      db.collection("users")
-        .insertOne({ email, password, tokeniv, aprovedStories: [] })
-        .then(() => resolve())
-        .catch((e) => reject(e));
+      bcrypt.hash(rawPassword, 10, (err, hash) => {
+        if (err) {
+          console.error(err)
+          reject(err);
+        } else {
+          db.collection("users")
+            .insertOne({ email, hash, aprovedStories: [] })
+            .then(() => resolve())
+            .catch((e) => reject(e));
+        }
+      });
     } catch (e) {
       console.error(e);
       reject(e);
@@ -26,10 +31,18 @@ exports.authenticate = (email, rawPassword) => {
         .findOne({ email: email })
         .then((user) => {
           if (user) {
-            if (cipher.decrypt(user.password, user.tokeniv) == rawPassword)
-              resolve({ _id: user._id });
-            else reject(new Error("username and password don't match"));
-          } else reject(new Error("user doesnt exist"));
+            bcrypt.compare(rawPassword, user.hash, (err, res) => {
+              if (err) {
+                console.error(err)
+                reject(err)
+              }
+              if (res) {
+                resolve({ _id: user._id })
+              } else {
+                reject(new Error("email or password incorrect"))
+              }
+            })
+          } else reject(new Error("email or password incorrect"));
         })
         .catch((e) => reject(e));
     } catch (e) {
