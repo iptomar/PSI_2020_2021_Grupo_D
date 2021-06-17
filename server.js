b75a277384f70379cecf1b746fc03b594277f5e7
NoPalm0il <jpedrocr2000@hotmail.com>
2021-06-17 19:03:36 +0100

    static deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 
+const path = require("path");
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
@@ -19,12 +20,17 @@ Promise.resolve(db.connectDB())
     app.use(cors());
     app.use(express.json());
 
+    app.use(express.static(path.resolve(__dirname, './frontend/build')));
     app.use("/api/user", authRoute);
     app.use("/api/map", mapRoute);
 
+    app.get('/', (_, res) => {
+      res.sendFile(path.resolve(__dirname, './frontend/build', 'index.html'));
+    });
+
     app.listen(port, () => {
       console.log(
-        `\x1b[34mSanta listening at http://localhost:${port}...\x1b[0m`
+        `\x1b[34mSanta listening at ${port}...\x1b[0m`
       );
     });
   })
